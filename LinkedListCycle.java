3fe3cadd22b986bb61db1d8fd30e4c55d5244a49
Akshay Bahadur <akshay.dominator@gmail.com>
2019-11-16 00:08:25 +0530

    Create LinkedListCycle.java

---
+++ LinkedListCycle.java
@@ -0,0 +1,57 @@
+/*
+
+Given a linked list, determine if it has a cycle in it.
+
+To represent a cycle in the given linked list, we use an integer pos which represents the position (0-indexed) in the linked list where tail connects to. If pos is -1, then there is no cycle in the linked list.
+
+ 
+
+Example 1:
+
+Input: head = [3,2,0,-4], pos = 1
+Output: true
+Explanation: There is a cycle in the linked list, where tail connects to the second node.
+
+Example 2:
+
+Input: head = [1,2], pos = 0
+Output: true
+Explanation: There is a cycle in the linked list, where tail connects to the first node.
+
+Example 3:
+
+Input: head = [1], pos = -1
+Output: false
+Explanation: There is no cycle in the linked list.
+
+Link : https://leetcode.com/problems/linked-list-cycle/
+
+*/
+
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        if (head == null)
+            return false;
+        Set<ListNode> lookupSet = new HashSet<>();
+        while(head.next!=null){
+            if (!lookupSet.contains(head))
+                lookupSet.add(head);
+            else
+                return true;
+            head = head.next;
+        }
+        return false;
+    }
+}
