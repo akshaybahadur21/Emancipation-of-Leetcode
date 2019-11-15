7e0c132e9438f407b6d671df954102a5df016f57
Akshay Bahadur <akshay.dominator@gmail.com>
2019-11-16 00:32:23 +0530

    Update LinkedListCycle.java

--- LinkedListCycle.java
+++ LinkedListCycle.java
@@ -55,3 +55,36 @@ public class Solution {
         return false;
     }
 }
+
+
+// 2 pointer
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null)
+            return false;
+        
+    ListNode slow = head;
+    ListNode fast = head.next;
+    while (slow != fast) {
+        if (fast == null || fast.next == null) {
+            return false;
+        }
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+    return true;
+        
+    }
+}
