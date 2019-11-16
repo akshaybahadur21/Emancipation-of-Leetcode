300e982ee54d00320f0b45a446528b1d0dd0466c
Akshay Bahadur <akshay.dominator@gmail.com>
2019-11-16 16:40:07 +0530

    Update ClminbingStairs.java

--- ClminbingStairs.java
+++ ClminbingStairs.java
@@ -48,4 +48,24 @@ class Solution {
         return res;
         }
     }
+    
+    //DP Tabulation
+    
+    class Solution {
+    public int climbStairs(int n) {
+        int []arr = new int[n+1];
+        return helperClimbStairs(n,arr);
+    }
+    
+    int helperClimbStairs(int n, int[]arr){
+        
+       if(n==0) return 0;
+        arr[0]=1;
+        arr[1]=1;
+        for(int i =2;i<=n;i++){
+            arr[i] = arr[i-2]+arr[i-1];  
+        }
+        return arr[n];
+}
+}
 }
