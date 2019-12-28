d73516988d19d000bda1f05e7d5de1dd72092f06
Akshay Bahadur <akshay.dominator@gmail.com>
2019-12-29 01:41:58 +0530

    Update 287-FindtheDuplicateNumber.java

--- 287-FindtheDuplicateNumber/287-FindtheDuplicateNumber.java
+++ 287-FindtheDuplicateNumber/287-FindtheDuplicateNumber.java
@@ -39,3 +39,27 @@ class Solution {
         
     }
 }
+
+//Hare - Rabbit
+class Solution {
+    public int findDuplicate(int[] nums) {
+        if (nums.length > 0){
+            int slow = nums[0];
+            int fast = nums[nums[0]];
+            while(slow != fast){
+                slow = nums[slow];
+                fast = nums[nums[fast]];
+            }
+
+            fast = 0;
+            while (fast != slow)
+            {
+                fast = nums[fast];
+                slow = nums[slow];
+            }
+            return slow;
+        }
+        return -1;
+        
+    }
+}
