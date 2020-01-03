43af6b9112391e6258b1180a8b7fb46109785dea
Akshay Bahadur <akshay.dominator@gmail.com>
2020-01-03 19:28:27 +0530

    Update 101-SymmetricTree.java

--- 101-SymmetricTree/101-SymmetricTree.java
+++ 101-SymmetricTree/101-SymmetricTree.java
@@ -60,3 +60,39 @@ class Solution {
         return (symmetricTreeHelper(p.left,q.right)&&symmetricTreeHelper(p.right,q.left));
     }
 }
+
+//bfs using stack
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null)
+            return true;
+        return bfs(root);
+    }
+    private boolean bfs(TreeNode root){
+        Stack<TreeNode> s = new Stack<>();
+        s.push(root.left);
+        s.push(root.right);
+        while(!s.isEmpty()){
+            TreeNode p = s.pop();
+            TreeNode q = s.pop();
+            if (p == null && q == null) 
+                continue;
+            if (p == null && q!=null || p != null && q == null || p.val != q.val)
+                return false;
+            s.push(p.left);
+            s.push(q.right);
+            s.push(p.right);
+            s.push(q.left);
+        }
+        return true;
+    }
+}
