2f1a47cac1a720ce57691de11723fe50fe103c38
Akshay Bahadur <akshay.dominator@gmail.com>
2020-02-18 23:27:35 +0530

    Create 160-IntersectionofTwoLinkedLists.java

---
+++ 160-IntersectionofTwoLinkedLists/160-IntersectionofTwoLinkedLists.java
@@ -0,0 +1,93 @@
+/*
+
+160. Intersection of Two Linked Lists
+Easy
+
+Write a program to find the node at which the intersection of two singly linked lists begins.
+
+For example, the following two linked lists:
+
+begin to intersect at node c1.
+
+Example 1:
+
+Input: intersectVal = 8, listA = [4,1,8,4,5], listB = [5,0,1,8,4,5], skipA = 2, skipB = 3
+Output: Reference of the node with value = 8
+Input Explanation: The intersected node's value is 8 (note that this must not be 0 if the two lists intersect). From the head of A, it reads as [4,1,8,4,5]. From the head of B, it reads as [5,0,1,8,4,5]. There are 2 nodes before the intersected node in A; There are 3 nodes before the intersected node in B.
+ 
+Example 2:
+
+Input: intersectVal = 2, listA = [0,9,1,2,4], listB = [3,2,4], skipA = 3, skipB = 1
+Output: Reference of the node with value = 2
+Input Explanation: The intersected node's value is 2 (note that this must not be 0 if the two lists intersect). From the head of A, it reads as [0,9,1,2,4]. From the head of B, it reads as [3,2,4]. There are 3 nodes before the intersected node in A; There are 1 node before the intersected node in B.
+
+Example 3:
+
+Input: intersectVal = 0, listA = [2,6,4], listB = [1,5], skipA = 3, skipB = 2
+Output: null
+Input Explanation: From the head of A, it reads as [2,6,4]. From the head of B, it reads as [1,5]. Since the two lists do not intersect, intersectVal must be 0, while skipA and skipB can be arbitrary values.
+Explanation: The two lists do not intersect, so return null.
+
+Notes:
+
+    If the two linked lists have no intersection at all, return null.
+    The linked lists must retain their original structure after the function returns.
+    You may assume there are no cycles anywhere in the entire linked structure.
+    Your code should preferably run in O(n) time and use only O(1) memory.
+
+*/
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        //add one linkedList into set and check for intersection in the 2nd linkedList
+        Set<ListNode> lookupSet = new HashSet<>();
+        while(headA != null){
+            lookupSet.add(headA);
+            headA = headA.next;
+        }
+        while(headB != null){
+            if (lookupSet.contains(headB))
+                return headB;
+            else
+                lookupSet.add(headB);
+            headB = headB.next;
+        }
+        return null;
+    }
+}
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        //no need to get the difference in length, just traverse until 1 is null and then simply point to the other head
+        if (headA == null || headB == null) return null;
+        ListNode a = headA;
+        ListNode b = headB;
+        while(a != b){
+            a = a == null ? headB : a.next;
+            b = b == null ? headA : b.next; 
+        }
+        return a;
+    }
+}
