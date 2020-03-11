5ba24987ac5ee6797aa77960cd74bd06f3052604
Akshay Bahadur <akshay.dominator@gmail.com>
2020-03-12 02:32:40 +0530

    Update 1315-SumofNodeswithEven-ValuedGrandparent.java

--- 1315-SumofNodeswithEven-ValuedGrandparent/1315-SumofNodeswithEven-ValuedGrandparent.java
+++ 1315-SumofNodeswithEven-ValuedGrandparent/1315-SumofNodeswithEven-ValuedGrandparent.java
@@ -61,3 +61,37 @@ class Solution {
         return res;
     }
 }
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    int sum = 0;
+    public int sumEvenGrandparent(TreeNode root) {
+        //Approach 2
+        // DFS
+        dfs(root);
+        return sum;
+    }
+    private void dfs(TreeNode node){
+        if (node == null) return;
+        if(node.val %2 == 0){
+            if (node.left != null && node.left.left != null)
+                sum+= node.left.left.val;
+            if (node.left != null && node.left.right != null)
+              sum += node.left.right.val;
+            if (node.right != null && node.right.left != null)
+                 sum += node.right.left.val;
+            if (node.right != null && node.right.right != null)
+                sum += node.right.right.val;
+        }
+        dfs(node.left);
+        dfs(node.right);
+    }
+}
