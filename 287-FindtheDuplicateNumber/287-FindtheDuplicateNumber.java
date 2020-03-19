058e073eaa3fa3f84a5c27e211ff06a8df5b3305
Akshay Bahadur <akshay.dominator@gmail.com>
2020-03-19 22:58:58 +0530

    Update 287-FindtheDuplicateNumber.java

--- 287-FindtheDuplicateNumber/287-FindtheDuplicateNumber.java
+++ 287-FindtheDuplicateNumber/287-FindtheDuplicateNumber.java
@@ -63,3 +63,15 @@ class Solution {
         
     }
 }
+
+class Solution {
+    public int findDuplicate(int[] nums) {
+        //mutate the array itself since you have values between 1..n
+        for (int i = 0; i < nums.length; i++){
+            if (nums[Math.abs(nums[i]) - 1] < 0)
+                return Math.abs(nums[i]);
+            nums[Math.abs(nums[i]) - 1] = -nums[Math.abs(nums[i]) - 1];
+        }
+        return -1;
+    }
+}
