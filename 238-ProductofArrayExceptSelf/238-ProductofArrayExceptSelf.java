19ec096cceaa66b9525a9860811bed2607118d4e
Akshay Bahadur <akshay.dominator@gmail.com>
2020-04-15 12:50:18 +0530

    Update 238-ProductofArrayExceptSelf.java

--- 238-ProductofArrayExceptSelf/238-ProductofArrayExceptSelf.java
+++ 238-ProductofArrayExceptSelf/238-ProductofArrayExceptSelf.java
@@ -45,3 +45,18 @@ class Solution {
         return res;
     }
 }
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int[] res = new int[nums.length];
+        res[0] = 1;
+        for (int i = 1; i < nums.length; i++) {
+            res[i] = res[i - 1] * nums[i - 1];
+        }
+        int right = 1;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            res[i] *= right;
+            right *= nums[i];
+        }
+        return res;
+    }
+}
