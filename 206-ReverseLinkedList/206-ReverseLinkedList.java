eb288b3a7228d9fd61216be074a1cfbfeeef7fd7
Akshay Bahadur <akshay.dominator@gmail.com>
2020-05-13 20:37:45 +0530

    Update 206-ReverseLinkedList.java

--- 206-ReverseLinkedList/206-ReverseLinkedList.java
+++ 206-ReverseLinkedList/206-ReverseLinkedList.java
@@ -74,3 +74,17 @@ class Solution {
 
     }
 }
+
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        while(curr != null){
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+}
