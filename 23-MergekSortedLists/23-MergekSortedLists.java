000055c5f123f1403b67a0db3a30c2f4bd435de3
Akshay Bahadur <akshay.dominator@gmail.com>
2020-06-10 19:15:12 +0530

    Update 23-MergekSortedLists.java

--- 23-MergekSortedLists/23-MergekSortedLists.java
+++ 23-MergekSortedLists/23-MergekSortedLists.java
@@ -52,3 +52,41 @@ class Solution {
         return pseudoHead.next;
     }
 }
+
+// Approach 2
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        if(lists == null || lists.length == 0) return null;
+        return partition(lists, 0, lists.length - 1);
+    }
+    private ListNode partition(ListNode[] lists, int lo, int hi){
+        if(lo >= hi) return lists[lo];
+        int mid = lo + (hi - lo) / 2;
+        ListNode l1 = partition(lists, lo, mid);
+        ListNode l2 = partition(lists, mid + 1, hi);
+        return merge(l1, l2);
+    }
+    private ListNode merge(ListNode l1, ListNode l2){
+        if(l1 == null) return l2;
+        if(l2 == null) return l1;
+        if(l1.val < l2.val){
+            l1.next = merge(l1.next, l2);
+            return l1;
+        }
+        else{
+            l2.next = merge(l1, l2.next);
+            return l2;
+        }
+        
+    }
+}
