1928c0ba613fdb2dc5ec7bac151e0ec31b3a4160
Akshay Bahadur <akshay.dominator@gmail.com>
2020-06-23 02:14:30 +0530

    Create 814-BinaryTreePruning.java

---
+++ 814-BinaryTreePruning/814-BinaryTreePruning.java
@@ -0,0 +1,65 @@
+/*
+
+814. Binary Tree Pruning
+Medium
+
+We are given the head node root of a binary tree, where additionally every node's value is either a 0 or a 1.
+
+Return the same tree where every subtree (of the given tree) not containing a 1 has been removed.
+
+(Recall that the subtree of a node X is X, plus every node that is a descendant of X.)
+
+Example 1:
+Input: [1,null,0,0,1]
+Output: [1,null,0,null,1]
+ 
+Explanation: 
+Only the red nodes satisfy the property "every subtree not containing a 1".
+The diagram on the right represents the answer.
+
+Example 2:
+Input: [1,0,1,0,0,0,1]
+Output: [1,null,1,null,1]
+
+Example 3:
+Input: [1,1,0,1,1,0,1,0]
+Output: [1,1,0,1,1,null,1]
+
+
+Note:
+
+    The binary tree will have at most 100 nodes.
+    The value of each node will only be 0 or 1.
+
+*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode pruneTree(TreeNode root) {
+        return dfs(root);
+    }
+    
+    private TreeNode dfs(TreeNode node){
+        if(node == null) return null;
+        node.left = dfs(node.left);
+        node.right = dfs(node.right);
+        if(node.left == null && node.right == null && node.val == 0)
+            return null;
+        return node;
+        
+    }
+}
