9eedfe03749354cd60ba7f3e854f3f1fd77a7a16
Akshay Bahadur <akshay.dominator@gmail.com>
2020-06-25 12:37:24 +0530

    Update 173-BinarySearchTreeIterator.java

--- 173-BinarySearchTreeIterator/173-BinarySearchTreeIterator.java
+++ 173-BinarySearchTreeIterator/173-BinarySearchTreeIterator.java
@@ -73,3 +73,51 @@ class BSTIterator {
  * int param_1 = obj.next();
  * boolean param_2 = obj.hasNext();
  */
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class BSTIterator {
+    Stack<TreeNode> s = new Stack<>();
+    public BSTIterator(TreeNode root) {
+        while(root != null){
+            s.push(root);
+            root = root.left;
+        }
+    }
+    
+    /** @return the next smallest number */
+    public int next() {
+        TreeNode curr = s.pop();
+        TreeNode temp = curr.right;
+        while(temp != null){
+            s.push(temp);
+            temp = temp.left;
+        }
+        return curr.val;
+    }
+    
+    /** @return whether we have a next smallest number */
+    public boolean hasNext() {
+        return !s.isEmpty();
+    }
+}
+
+/**
+ * Your BSTIterator object will be instantiated and called as such:
+ * BSTIterator obj = new BSTIterator(root);
+ * int param_1 = obj.next();
+ * boolean param_2 = obj.hasNext();
+ */
