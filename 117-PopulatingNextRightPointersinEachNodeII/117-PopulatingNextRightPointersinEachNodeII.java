d1ac03fda0a0669a98620160ac4808509774467d
Akshay Bahadur <akshay.dominator@gmail.com>
2020-08-04 16:53:02 +0530

    Update 117-PopulatingNextRightPointersinEachNodeII.java

--- 117-PopulatingNextRightPointersinEachNodeII/117-PopulatingNextRightPointersinEachNodeII.java
+++ 117-PopulatingNextRightPointersinEachNodeII/117-PopulatingNextRightPointersinEachNodeII.java
@@ -129,3 +129,50 @@ class Solution {
         
     }
 }
+
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+    
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+*/
+
+class Solution {
+    public Node connect(Node root) {
+        Node first = root;
+        while(root != null){
+            Node temp = new Node(0);
+            Node cursor = temp;
+            while(root != null){
+                if(root.left != null){
+                    cursor.next = root.left;
+                    cursor = cursor.next;
+                }
+                if(root.right != null){
+                    cursor.next = root.right;
+                    cursor = cursor.next;
+                }
+                root = root.next;
+            }
+            root = temp.next;
+            temp.next = null;
+        }
+        return first;
+    }
+}
