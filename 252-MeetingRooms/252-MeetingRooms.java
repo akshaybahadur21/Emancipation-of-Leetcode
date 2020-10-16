65d9a5425a4de37b7c4d9bf0b430914c704c046c
Akshay Bahadur <akshay.dominator@gmail.com>
2020-10-17 01:28:25 +0530

    Update 252-MeetingRooms.java

--- 252-MeetingRooms/252-MeetingRooms.java
+++ 252-MeetingRooms/252-MeetingRooms.java
@@ -17,6 +17,20 @@ Output: true
 
 class Solution {
     public boolean canAttendMeetings(int[][] intervals) {
+        if(intervals == null || intervals.length == 0) return true;
+        Arrays.sort(intervals, (n1, n2) -> n1[0] - n2[0]);
+        int[] first = intervals[0];
+        for(int i = 1; i < intervals.length; i++){
+            int[] second = intervals[i];
+            if(first[1] > second[0]) return false;
+            first = new int[]{Math.min(first[0], second[0]), Math.max(first[1], second[1])};
+        }
+        return true;
+    }
+}
+
+class Solution {
+    public boolean canAttendMeetings(int[][] intervals) {
         Arrays.sort(intervals, (i1, i2) -> i1[0]-i2[0]);
         for (int i = 0; i<intervals.length-1; i++){
             if (intervals[i][1] > intervals[i + 1][0])
