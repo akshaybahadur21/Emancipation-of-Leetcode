49e3343cacd905df5fdfc8afee62829bb3e81039
Akshay Bahadur <akshay.dominator@gmail.com>
2020-12-06 13:12:42 +0530

    Create 1679-MaxNumberofK-SumPairs.java

---
+++ 1679-MaxNumberofK-SumPairs/1679-MaxNumberofK-SumPairs.java
@@ -0,0 +1,53 @@
+/*
+
+1679. Max Number of K-Sum Pairs
+Medium
+
+You are given an integer array nums and an integer k.
+
+In one operation, you can pick two numbers from the array whose sum equals k and remove them from the array.
+
+Return the maximum number of operations you can perform on the array.
+
+Example 1:
+
+Input: nums = [1,2,3,4], k = 5
+Output: 2
+Explanation: Starting with nums = [1,2,3,4]:
+- Remove numbers 1 and 4, then nums = [2,3]
+- Remove numbers 2 and 3, then nums = []
+There are no more pairs that sum up to 5, hence a total of 2 operations.
+
+Example 2:
+
+Input: nums = [3,1,3,4,3], k = 6
+Output: 1
+Explanation: Starting with nums = [3,1,3,4,3]:
+- Remove the first two 3's, then nums = [1,4,3]
+There are no more pairs that sum up to 6, hence a total of 1 operation.
+
+Constraints:
+
+    1 <= nums.length <= 105
+    1 <= nums[i] <= 109
+    1 <= k <= 109
+
+*/
+
+class Solution {
+    public int maxOperations(int[] nums, int k) {
+        Arrays.sort(nums);
+        int lo = 0, hi = nums.length - 1, res = 0;
+        while(lo < hi){
+            if(nums[lo] + nums[hi] == k){
+                res++;
+                lo++;
+                hi--;
+            }
+            else if(nums[lo] + nums[hi] > k)
+                hi--;
+            else lo ++;
+        }
+        return res;
+    }
+}
