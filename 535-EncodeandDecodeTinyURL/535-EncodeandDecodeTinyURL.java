e958a6a1fb16e361112c4a2131ac0abe102c3061
Akshay Bahadur <akshay.dominator@gmail.com>
2021-03-15 23:11:07 +0530

    Create 535-EncodeandDecodeTinyURL.java

---
+++ 535-EncodeandDecodeTinyURL/535-EncodeandDecodeTinyURL.java
@@ -0,0 +1,29 @@
+/*
+
+535. Encode and Decode TinyURL
+Medium
+
+Note: This is a companion problem to the System Design problem: Design TinyURL.
+
+TinyURL is a URL shortening service where you enter a URL such as https://leetcode.com/problems/design-tinyurl and it returns a short URL such as http://tinyurl.com/4e9iAk.
+
+Design the encode and decode methods for the TinyURL service. There is no restriction on how your encode/decode algorithm should work. You just need to ensure that a URL can be encoded to a tiny URL and the tiny URL can be decoded to the original URL.
+
+*/
+
+public class Codec {
+
+    // Encodes a URL to a shortened URL.
+    public String encode(String longUrl) {
+        return longUrl;
+    }
+
+    // Decodes a shortened URL to its original URL.
+    public String decode(String shortUrl) {
+        return shortUrl;
+    }
+}
+
+// Your Codec object will be instantiated and called as such:
+// Codec codec = new Codec();
+// codec.decode(codec.encode(url));
