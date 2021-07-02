7bcd03babb2f35f79c94871a0600ff628fdb6362
Akshay Bahadur <akshaybahadur21@gmail.com>
2021-07-02 17:51:36 +0530

    Create 5818-LongestCommonSubsequenceBetweenSortedArrays.java

---
+++ 5818-LongestCommonSubsequenceBetweenSortedArrays/5818-LongestCommonSubsequenceBetweenSortedArrays.java
@@ -0,0 +1,58 @@
+/*
+
+5818. Longest Common Subsequence Between Sorted Arrays
+Medium
+
+Given an array of integer arrays arrays where each arrays[i] is sorted in strictly increasing order, return an integer array representing the longest common subsequence between all the arrays.
+
+A subsequence is a sequence that can be derived from another sequence by deleting some or no elements without changing the order of the remaining elements.
+
+Example 1:
+
+Input: arrays = [[1,3,4],
+               [1,4,7,9]]
+Output: [1,4]
+Explanation: The longest subsequence which in the two arrays is [1,4].
+
+Example 2:
+
+Input: arrays = [[2,3,6,8],
+               [1,2,3,5,6,7,10],
+               [2,3,4,6,9]]
+Output: [2,3,6]
+Explanation: The longest subsequence which in all of the three arrays is [2,3,6].
+
+Example 3:
+
+Input: arrays = [[1,2,3,4,5],
+               [6,7,8]]
+Output: []
+Explanation: There is no common subsequence between the two arrays.
+
+Constraints:
+
+    2 <= arrays.length <= 100
+    1 <= arrays[i].length <= 100
+    1 <= arrays[i][j] <= 100
+    arrays[i] is sorted in strictly increasing order.
+
+*/
+
+class Solution {
+    public List<Integer> longestCommomSubsequence(int[][] arrays) {
+        // Use a simple map to get count of each element from each array
+        // if count == number of array, add it to list
+        Map<Integer, Integer> map = new HashMap<>();
+        List<Integer> resList = new ArrayList<>();
+        for(int i = 0; i < arrays.length; i++){
+            int[] arr = arrays[i];
+            for(int a : arr){
+                if(i == arrays.length - 1){
+                    if(map.containsKey(a) && map.get(a) == arrays.length - 1) resList.add(a);
+                }
+                else map.put(a, map.containsKey(a) ? map.get(a) + 1 : 1 );
+            }
+        }
+        return resList;
+    }
+}
