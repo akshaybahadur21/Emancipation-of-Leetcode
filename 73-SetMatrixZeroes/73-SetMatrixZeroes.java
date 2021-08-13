147f2b0c6ac222f9125fc7a357307c0d1550ae39
Akshay Bahadur <akshaybahadur21@gmail.com>
2021-08-13 10:23:43 -0400

    Create 73-SetMatrixZeroes.java

---
+++ 73-SetMatrixZeroes/73-SetMatrixZeroes.java
@@ -0,0 +1,54 @@
+/*
+
+73. Set Matrix Zeroes
+Medium
+
+Given an m x n integer matrix matrix, if an element is 0, set its entire row and column to 0's, and return the matrix.
+
+You must do it in place.
+
+Example 1:
+
+Input: matrix = [[1,1,1],[1,0,1],[1,1,1]]
+Output: [[1,0,1],[0,0,0],[1,0,1]]
+
+Example 2:
+
+Input: matrix = [[0,1,2,0],[3,4,5,2],[1,3,1,5]]
+Output: [[0,0,0,0],[0,4,5,0],[0,3,1,0]]
+
+Constraints:
+
+    m == matrix.length
+    n == matrix[0].length
+    1 <= m, n <= 200
+    -231 <= matrix[i][j] <= 231 - 1
+
+Follow up:
+
+    A straightforward solution using O(mn) space is probably a bad idea.
+    A simple improvement uses O(m + n) space, but still not the best solution.
+    Could you devise a constant space solution?
+
+*/
+
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        Set<Integer> setx = new HashSet<>();
+        Set<Integer> sety = new HashSet<>();
+        for(int i = 0; i < matrix.length; i++){
+            for(int j = 0; j < matrix[0].length; j++){
+                if(matrix[i][j] == 0){
+                    setx.add(i);
+                    sety.add(j);
+                }
+            }
+        }
+        for(int i = 0; i < matrix.length; i++){
+            for(int j = 0; j < matrix[0].length; j++){
+                if(setx.contains(i)) matrix[i][j] = 0;
+                if(sety.contains(j)) matrix[i][j] = 0;
+            }
+        }
+    }
+}
