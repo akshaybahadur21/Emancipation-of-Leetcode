da35a0c0c084cd06fb5a9b1debb833c6f04a43a1
Akshay Bahadur <akshaybahadur21@gmail.com>
2021-08-16 17:23:37 -0400

    Create 303-RangeSumQueryImmutable.java

---
+++ 303-RangeSumQueryImmutable/303-RangeSumQueryImmutable.java
@@ -0,0 +1,58 @@
+/*
+
+303. Range Sum Query - Immutable
+Easy
+
+Given an integer array nums, handle multiple queries of the following type:
+
+    Calculate the sum of the elements of nums between indices left and right inclusive where left <= right.
+
+Implement the NumArray class:
+
+    NumArray(int[] nums) Initializes the object with the integer array nums.
+    int sumRange(int left, int right) Returns the sum of the elements of nums between indices left and right inclusive (i.e. nums[left] + nums[left + 1] + ... + nums[right]).
+
+Example 1:
+
+Input
+["NumArray", "sumRange", "sumRange", "sumRange"]
+[[[-2, 0, 3, -5, 2, -1]], [0, 2], [2, 5], [0, 5]]
+Output
+[null, 1, -1, -3]
+
+Explanation
+NumArray numArray = new NumArray([-2, 0, 3, -5, 2, -1]);
+numArray.sumRange(0, 2); // return (-2) + 0 + 3 = 1
+numArray.sumRange(2, 5); // return 3 + (-5) + 2 + (-1) = -1
+numArray.sumRange(0, 5); // return (-2) + 0 + 3 + (-5) + 2 + (-1) = -3
+
+Constraints:
+
+    1 <= nums.length <= 104
+    -105 <= nums[i] <= 105
+    0 <= left <= right < nums.length
+    At most 104 calls will be made to sumRange.
+
+*/
+
+class NumArray {
+    Map<Integer, Integer> map = new HashMap<>();
+    public NumArray(int[] nums) {
+        int curr = 0;
+        for(int i = 0; i < nums.length; i++){
+            curr += nums[i];
+            map.put(i, curr);
+        }
+    }
+    
+    public int sumRange(int left, int right) {
+        if(left == 0) return map.get(right);
+        return map.get(right) - map.get(left - 1);
+    }
+}
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray obj = new NumArray(nums);
+ * int param_1 = obj.sumRange(left,right);
+ */
