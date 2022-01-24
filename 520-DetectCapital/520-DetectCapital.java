b693408804d2b4f51df394214747c84b63fb10a3
Akshay Bahadur <akshaybahadur21@gmail.com>
2022-01-24 23:19:38 +0530

    Update 520-DetectCapital.java

--- 520-DetectCapital/520-DetectCapital.java
+++ 520-DetectCapital/520-DetectCapital.java
@@ -42,3 +42,16 @@ class Solution {
         return false;
     }
 }
+
+class Solution {
+    public boolean detectCapitalUse(String word) {
+        if(word.length() == 1) return true;
+        int count = 0, first = 0;
+        for(int i = 0; i < word.length(); i++){
+            if(i == 0 && word.charAt(i) >= 'A' & word.charAt(i) <= 'Z') first++;
+            char ch = word.charAt(i);
+            if(ch >= 'a' && ch <= 'z') count++;
+        }
+        return count == word.length() || count == 0 || count + first == word.length();
+    }
+}
