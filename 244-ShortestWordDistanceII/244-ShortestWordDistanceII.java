b7d3dcf8a83b758c1db2c8b48db6c8a4fac85b61
Akshay Bahadur <akshaybahadur21@gmail.com>
2022-06-17 02:23:31 -0400

    Create 244-ShortestWordDistanceII.java

---
+++ 244-ShortestWordDistanceII/244-ShortestWordDistanceII.java
@@ -0,0 +1,66 @@
+/*
+
+244. Shortest Word Distance II
+
+Design a data structure that will be initialized with a string array, and then it should answer queries of the shortest distance between two different strings from the array.
+
+Implement the WordDistance class:
+
+WordDistance(String[] wordsDict) initializes the object with the strings array wordsDict.
+int shortest(String word1, String word2) returns the shortest distance between word1 and word2 in the array wordsDict.
+ 
+
+Example 1:
+
+Input
+["WordDistance", "shortest", "shortest"]
+[[["practice", "makes", "perfect", "coding", "makes"]], ["coding", "practice"], ["makes", "coding"]]
+Output
+[null, 3, 1]
+
+Explanation
+WordDistance wordDistance = new WordDistance(["practice", "makes", "perfect", "coding", "makes"]);
+wordDistance.shortest("coding", "practice"); // return 3
+wordDistance.shortest("makes", "coding");    // return 1
+ 
+
+Constraints:
+
+1 <= wordsDict.length <= 3 * 104
+1 <= wordsDict[i].length <= 10
+wordsDict[i] consists of lowercase English letters.
+word1 and word2 are in wordsDict.
+word1 != word2
+At most 5000 calls will be made to shortest.
+
+*/
+
+class WordDistance {
+    Map<String, List<Integer>> map = new HashMap<>();
+    int count;
+    public WordDistance(String[] wordsDict) {
+        this.count = 0;
+        for(String w: wordsDict){
+            map.putIfAbsent(w, new ArrayList<>());
+            map.get(w).add(count++);
+        }
+    }
+    
+    public int shortest(String word1, String word2) {
+        List<Integer> l1 = map.get(word1);
+        List<Integer> l2 = map.get(word2);
+        int min = Integer.MAX_VALUE;
+        for(int i = 0; i < l1.size(); i++){
+            for(int j = 0; j < l2.size(); j++){
+                min = Math.min(min, Math.abs(l1.get(i) - l2.get(j)));
+            }
+        }
+        return min;
+    }
+}
+
+/**
+ * Your WordDistance object will be instantiated and called as such:
+ * WordDistance obj = new WordDistance(wordsDict);
+ * int param_1 = obj.shortest(word1,word2);
+ */
