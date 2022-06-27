b2bc24d7ec0a3553bce0e8eefc7c86e74a8db266
Akshay Bahadur <akshaybahadur21@gmail.com>
2022-06-27 16:45:27 -0400

    Create 796-RotateString.java

---
+++ 796-RotateString/796-RotateString.java
@@ -0,0 +1,33 @@
+/*
+
+796. Rotate String
+
+Given two strings s and goal, return true if and only if s can become goal after some number of shifts on s.
+
+A shift on s consists of moving the leftmost character of s to the rightmost position.
+
+For example, if s = "abcde", then it will be "bcdea" after one shift.
+ 
+
+Example 1:
+
+Input: s = "abcde", goal = "cdeab"
+Output: true
+Example 2:
+
+Input: s = "abcde", goal = "abced"
+Output: false
+ 
+
+Constraints:
+
+1 <= s.length, goal.length <= 100
+s and goal consist of lowercase English letters.
+
+*/
+
+class Solution {
+    public boolean rotateString(String A, String B) {
+        return A.length() == B.length() && (A + A).contains(B);
+    }
+}
