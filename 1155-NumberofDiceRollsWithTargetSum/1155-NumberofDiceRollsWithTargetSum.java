edf1a2d26e47bfcd74a6f6bc68d17ef28739b3ee
Akshay Bahadur <akshaybahadur21@gmail.com>
2022-07-18 13:59:54 -0400

    Create 1155-NumberofDiceRollsWithTargetSum.java

---
+++ 1155-NumberofDiceRollsWithTargetSum/1155-NumberofDiceRollsWithTargetSum.java
@@ -0,0 +1,50 @@
+/*
+
+1155. Number of Dice Rolls With Target Sum
+
+You have n dice and each die has k faces numbered from 1 to k.
+
+Given three integers n, k, and target, return the number of possible ways (out of the kn total ways) to roll the dice so the sum of the face-up numbers equals target. Since the answer may be too large, return it modulo 109 + 7.
+
+Example 1:
+
+Input: n = 1, k = 6, target = 3
+Output: 1
+Explanation: You throw one die with 6 faces.
+There is only one way to get a sum of 3.
+Example 2:
+
+Input: n = 2, k = 6, target = 7
+Output: 6
+Explanation: You throw two dice, each with 6 faces.
+There are 6 ways to get a sum of 7: 1+6, 2+5, 3+4, 4+3, 5+2, 6+1.
+Example 3:
+
+Input: n = 30, k = 30, target = 500
+Output: 222616187
+Explanation: The answer must be returned modulo 109 + 7.
+
+Constraints:
+
+1 <= n, k <= 30
+1 <= target <= 1000
+
+*/
+
+class Solution {
+    int mod = 1000000007;
+    public int numRollsToTarget(int n, int k, int target) {
+        return dfs(n, k, target, new Long[n + 1][target + 1]);
+    }
+    private int dfs(int n, int k, int target, Long[][] cache){
+        if(target < 0 || n < 0) return 0;
+        if(target == 0 && n == 0) return 1;
+        if(cache[n][target] != null) return (cache[n][target]).intValue();
+        long count = 0;
+        for(int i = 1; i <= k; i++){
+            count += dfs(n - 1, k, target - i, cache);
+        }
+        cache[n][target] = count%mod;
+        return cache[n][target].intValue();
+    }
+}
