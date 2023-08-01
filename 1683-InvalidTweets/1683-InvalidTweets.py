35ee74ccd85b1d132ae7bc137d62a467c4c25322
Akshay Bahadur <akshaybahadur21@gmail.com>
2023-07-31 22:22:33 -0700

    Create 1683-InvalidTweets.py

---
+++ 1683-InvalidTweets/1683-InvalidTweets.py
@@ -0,0 +1,54 @@
+'''
+
+1683. Invalid Tweets
+Easy
+SQL Schema
+
+Table: Tweets
+
++----------------+---------+
+| Column Name    | Type    |
++----------------+---------+
+| tweet_id       | int     |
+| content        | varchar |
++----------------+---------+
+In SQL, tweet_id is the primary key for this table.
+This table contains all the tweets in a social media app.
+
+ 
+
+Find the IDs of the invalid tweets. The tweet is invalid if the number of characters used in the content of the tweet is strictly greater than 15.
+
+Return the result table in any order.
+
+The result format is in the following example.
+ 
+
+Example 1:
+
+Input: 
+Tweets table:
++----------+----------------------------------+
+| tweet_id | content                          |
++----------+----------------------------------+
+| 1        | Vote for Biden                   |
+| 2        | Let us make America great again! |
++----------+----------------------------------+
+Output: 
++----------+
+| tweet_id |
++----------+
+| 2        |
++----------+
+Explanation: 
+Tweet 1 has length = 14. It is a valid tweet.
+Tweet 2 has length = 32. It is an invalid tweet.
+
+
+'''
+
+import pandas as pd
+
+def invalid_tweets(tweets: pd.DataFrame) -> pd.DataFrame:
+    tweets = tweets[tweets['content'].str.len() > 15]
+    return tweets[['tweet_id']]
