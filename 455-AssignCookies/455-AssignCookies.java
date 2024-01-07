d2dd77e71d639f709dac3cd55e959ed79b288a6f
Akshay Bahadur <akshaybahadur21@gmail.com>
2024-01-07 12:04:03 -0800

    Update 455-AssignCookies.java

--- 455-AssignCookies/455-AssignCookies.java
+++ 455-AssignCookies/455-AssignCookies.java
@@ -54,3 +54,22 @@ class Solution {
         
     }
 }
+
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int i = g.length - 1;
+        int j = s.length - 1;
+        int res = 0;
+        while(i >= 0 && j >= 0){
+            if(g[i] <= s[j]){
+                res++;
+                i--;
+                j--;
+            }
+            else i--;
+        }
+        return res;
+    }
+}
