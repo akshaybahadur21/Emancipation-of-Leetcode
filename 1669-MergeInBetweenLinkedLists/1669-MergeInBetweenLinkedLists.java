237590967be4fb8a1445f49f38ff7fbd2a0e3d82
Akshay Bahadur <akshaybahadur21@gmail.com>
2024-03-20 09:00:54 -0700

    Create 1669-MergeInBetweenLinkedLists.java

---
+++ 1669-MergeInBetweenLinkedLists/1669-MergeInBetweenLinkedLists.java
@@ -0,0 +1,61 @@
+/*
+
+1669. Merge In Between Linked Lists
+
+You are given two linked lists: list1 and list2 of sizes n and m respectively.
+
+Remove list1's nodes from the ath node to the bth node, and put list2 in their place.
+
+The blue edges and nodes in the following figure indicate the result:
+
+Build the result list and return its head.
+
+Example 1:
+
+Input: list1 = [10,1,13,6,9,5], a = 3, b = 4, list2 = [1000000,1000001,1000002]
+Output: [10,1,13,1000000,1000001,1000002,5]
+Explanation: We remove the nodes 3 and 4 and put the entire list2 in their place. The blue edges and nodes in the above figure indicate the result.
+
+Example 2:
+
+Input: list1 = [0,1,2,3,4,5,6], a = 2, b = 5, list2 = [1000000,1000001,1000002,1000003,1000004]
+Output: [0,1,1000000,1000001,1000002,1000003,1000004,6]
+Explanation: The blue edges and nodes in the above figure indicate the result.
+
+Constraints:
+
+    3 <= list1.length <= 104
+    1 <= a <= b < list1.length - 1
+    1 <= list2.length <= 104
+
+*/
+
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeInBetween(ListNode list1, int a, int b, ListNode list2) {
+        ListNode newHead = new ListNode(-1);
+        newHead.next = list1;
+        ListNode prev = newHead, temp = list1;
+        int counter = 0;
+        while(counter++ != a){
+            prev = temp;
+            temp = temp.next;
+        }
+        prev.next = null;
+        prev.next = list2;
+        while(counter++ != b + 1) temp = temp.next;
+        while(prev.next != null) prev = prev.next;
+        prev.next = temp.next;
+        return newHead.next;
+    }
+}
