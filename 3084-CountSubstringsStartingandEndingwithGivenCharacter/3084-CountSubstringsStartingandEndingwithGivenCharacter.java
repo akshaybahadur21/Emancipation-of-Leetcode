421477d5c709dc035efe877f4a9552c80abf04e6
Akshay Bahadur <akshaybahadur21@gmail.com>
2024-03-25 11:41:13 -0700

    Update 3084-CountSubstringsStartingandEndingwithGivenCharacter.java

--- 3084-CountSubstringsStartingandEndingwithGivenCharacter/3084-CountSubstringsStartingandEndingwithGivenCharacter.java
+++ 3084-CountSubstringsStartingandEndingwithGivenCharacter/3084-CountSubstringsStartingandEndingwithGivenCharacter.java
@@ -44,3 +44,14 @@ class Solution {
         return res;
     }
 }
+
+
+class Solution {
+    public long countSubstrings(String s, char c) {
+        long count = 0;
+        for(char ch : s.toCharArray()){
+            if(ch == c) count++;
+        }
+        return count * (count + 1) / 2;
+    }
+}
