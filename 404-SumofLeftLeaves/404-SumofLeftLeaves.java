df4ff1071bd8b0a627693cfdd35c8cfb7cc2818d
Akshay Bahadur <akshaybahadur21@gmail.com>
2024-04-14 04:27:53 -0700

    Update 404-SumofLeftLeaves.java

--- 404-SumofLeftLeaves/404-SumofLeftLeaves.java
+++ 404-SumofLeftLeaves/404-SumofLeftLeaves.java
@@ -66,3 +66,34 @@ class Solution {
         return dfs(node.left, true) + dfs(node.right, false);
     }
 }
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int sumOfLeftLeaves(TreeNode root) {
+        int[] res = new int[]{0};
+        dfs(root, res, false);
+        return res[0];
+    }
+
+    private void dfs(TreeNode node, int[] res, boolean isLeft){
+        if(node == null) return;
+        if(node.left == null && node.right == null && isLeft) res[0] += node.val;
+        dfs(node.left, res, true);
+        dfs(node.right, res, false);
+    }
+}
