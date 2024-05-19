926c8073ab533ec4a93865627b4c3292463d889a
Akshay Bahadur <akshaybahadur21@gmail.com>
2024-05-19 16:27:51 -0700

    Create 100-SameTree.py

---
+++ 100-SameTree/100-SameTree.py
@@ -0,0 +1,48 @@
+"""
+
+100. Same Tree
+
+Given the roots of two binary trees p and q, write a function to check if they are the same or not.
+
+Two binary trees are considered the same if they are structurally identical, and the nodes have the same value.
+
+Example 1:
+
+Input: p = [1,2,3], q = [1,2,3]
+Output: true
+
+Example 2:
+
+Input: p = [1,2], q = [1,null,2]
+Output: false
+
+Example 3:
+
+Input: p = [1,2,1], q = [1,1,2]
+Output: false
+
+Constraints:
+
+    The number of nodes in both trees is in the range [0, 100].
+    -104 <= Node.val <= 104
+
+"""
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        def dfs(p, q):
+            if p is None and q is None:
+                return True
+            if p is None and q is not None or p is not None and q is None:
+                return False
+            if p.val != q.val:
+                return False
+            return dfs(p.left, q.left) and dfs(p.right, q.right)
+        return dfs(p, q)
