21e35611dcc92a084ef77bc2b3163a1a651408a7
Akshay Bahadur <akshaybahadur21@gmail.com>
2024-06-07 22:40:53 -0700

    Create 2083-SubstringsThatBeginandEndWiththeSameLetter.py

---
+++ 2083-SubstringsThatBeginandEndWiththeSameLetter/2083-SubstringsThatBeginandEndWiththeSameLetter.py
@@ -0,0 +1,50 @@
+"""
+
+2083. Substrings That Begin and End With the Same Letter
+
+You are given a 0-indexed string s consisting of only lowercase English letters. Return the number of substrings in s that begin and end with the same character.
+
+A substring is a contiguous non-empty sequence of characters within a string.
+
+Example 1:
+
+Input: s = "abcba"
+Output: 7
+Explanation:
+The substrings of length 1 that start and end with the same letter are: "a", "b", "c", "b", and "a".
+The substring of length 3 that starts and ends with the same letter is: "bcb".
+The substring of length 5 that starts and ends with the same letter is: "abcba".
+
+Example 2:
+
+Input: s = "abacad"
+Output: 9
+Explanation:
+The substrings of length 1 that start and end with the same letter are: "a", "b", "a", "c", "a", and "d".
+The substrings of length 3 that start and end with the same letter are: "aba" and "aca".
+The substring of length 5 that starts and ends with the same letter is: "abaca".
+
+Example 3:
+
+Input: s = "a"
+Output: 1
+Explanation:
+The substring of length 1 that starts and ends with the same letter is: "a".
+
+ 
+
+Constraints:
+
+    1 <= s.length <= 105
+    s consists only of lowercase English letters.
+
+"""
+
+class Solution:
+    def numberOfSubstrings(self, s: str) -> int:
+        _map = [0]*26
+        res = 0
+        for i in range(len(s)):
+            _map[ord(s[i]) - ord('a')] += 1
+            res +=  _map[ord(s[i]) - ord('a')]
+        return res
