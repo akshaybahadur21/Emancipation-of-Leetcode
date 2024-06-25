813d0f6194dd2ad11df28005c457f51066ba5c1e
Akshay Bahadur <akshaybahadur21@gmail.com>
2024-06-24 21:49:01 -0700

    Update 1038-BinarySearchTreetoGreaterSumTree.java

--- 1038-BinarySearchTreetoGreaterSumTree/1038-BinarySearchTreetoGreaterSumTree.java
+++ 1038-BinarySearchTreetoGreaterSumTree/1038-BinarySearchTreetoGreaterSumTree.java
@@ -52,3 +52,33 @@ class Solution {
         return root;
     }
 }
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode bstToGst(TreeNode root) {
+        int[] sum = new int[]{0};
+        dfs(root, sum);
+        return root;
+    }
+    private void dfs(TreeNode node, int[] sum){
+        if(node == null) return;
+        dfs(node.right, sum);
+        sum[0] += node.val;
+        node.val = sum[0];
+        dfs(node.left, sum);
+    }
+}
