4ebee590175151c1b2ce866cfd49b32e95c6070a
Akshay Bahadur <akshaybahadur21@gmail.com>
2024-07-03 23:36:06 -0700

    Create 2181-MergeNodesinBetweenZeros.java

---
+++ 2181-MergeNodesinBetweenZeros/2181-MergeNodesinBetweenZeros.java
@@ -0,0 +1,66 @@
+/*
+
+2181. Merge Nodes in Between Zeros
+
+You are given the head of a linked list, which contains a series of integers separated by 0's. The beginning and end of the linked list will have Node.val == 0.
+
+For every two consecutive 0's, merge all the nodes lying in between them into a single node whose value is the sum of all the merged nodes. The modified list should not contain any 0's.
+
+Return the head of the modified linked list.
+
+Example 1:
+
+Input: head = [0,3,1,0,4,5,2,0]
+Output: [4,11]
+Explanation: 
+The above figure represents the given linked list. The modified list contains
+- The sum of the nodes marked in green: 3 + 1 = 4.
+- The sum of the nodes marked in red: 4 + 5 + 2 = 11.
+
+Example 2:
+
+Input: head = [0,1,0,3,0,2,2,0]
+Output: [1,3,4]
+Explanation: 
+The above figure represents the given linked list. The modified list contains
+- The sum of the nodes marked in green: 1 = 1.
+- The sum of the nodes marked in red: 3 = 3.
+- The sum of the nodes marked in yellow: 2 + 2 = 4.
+
+Constraints:
+
+    The number of nodes in the list is in the range [3, 2 * 105].
+    0 <= Node.val <= 1000
+    There are no two consecutive nodes with Node.val == 0.
+    The beginning and end of the linked list have Node.val == 0.
+
+*/
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeNodes(ListNode head) {
+        ListNode newHead = new ListNode(0);
+        ListNode temp = newHead;
+        int curr = 0;
+        head = head.next;
+        while(head != null){
+            if(head.val == 0){
+                temp.next = new ListNode(curr);
+                curr = 0;
+                temp = temp.next;
+            }
+            else curr += head.val;
+            head = head.next;
+        }
+        return newHead.next;
+    }
+}
