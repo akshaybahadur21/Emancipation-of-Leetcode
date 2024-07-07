fee95d70de2d70b2039ee2f2950cdc8cac3e38d2
Akshay Bahadur <akshaybahadur21@gmail.com>
2024-07-07 00:04:42 -0700

    Create 849-MaximizeDistancetoClosestPerson.py

---
+++ 849-MaximizeDistancetoClosestPerson/849-MaximizeDistancetoClosestPerson.py
@@ -0,0 +1,53 @@
+"""
+
+849. Maximize Distance to Closest Person
+
+You are given an array representing a row of seats where seats[i] = 1 represents a person sitting in the ith seat, and seats[i] = 0 represents that the ith seat is empty (0-indexed).
+
+There is at least one empty seat, and at least one person sitting.
+
+Alex wants to sit in the seat such that the distance between him and the closest person to him is maximized. 
+
+Return that maximum distance to the closest person.
+
+Example 1:
+
+Input: seats = [1,0,0,0,1,0,1]
+Output: 2
+Explanation: 
+If Alex sits in the second open seat (i.e. seats[2]), then the closest person has distance 2.
+If Alex sits in any other open seat, the closest person has distance 1.
+Thus, the maximum distance to the closest person is 2.
+
+Example 2:
+
+Input: seats = [1,0,0,0]
+Output: 3
+Explanation: 
+If Alex sits in the last seat (i.e. seats[3]), the closest person is 3 seats away.
+This is the maximum distance possible, so the answer is 3.
+
+Example 3:
+
+Input: seats = [0,1]
+Output: 1
+
+Constraints:
+
+    2 <= seats.length <= 2 * 104
+    seats[i] is 0 or 1.
+    At least one seat is empty.
+    At least one seat is occupied.
+
+"""
+
+class Solution:
+    def maxDistToClosest(self, seats: List[int]) -> int:
+        lo, hi, _max = - 1, 0, 0
+        while hi < len(seats):
+            if seats[hi] == 1:
+                if lo == -1: _max = max(_max, hi)
+                else: _max = max(_max, (hi - lo) // 2)
+                lo = hi
+            hi += 1
+        return max(_max, len(seats) - lo - 1)
