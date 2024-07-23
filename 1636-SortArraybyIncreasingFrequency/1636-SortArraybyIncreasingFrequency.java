1e3cf7c9cee27a09faa3483d31a6ed401a6cb845
Akshay Bahadur <akshaybahadur21@gmail.com>
2024-07-22 21:59:57 -0700

    Create 1636-SortArraybyIncreasingFrequency.java

---
+++ 1636-SortArraybyIncreasingFrequency/1636-SortArraybyIncreasingFrequency.java
@@ -0,0 +1,42 @@
+/*
+
+1636. Sort Array by Increasing Frequency
+
+Given an array of integers nums, sort the array in increasing order based on the frequency of the values. If multiple values have the same frequency, sort them in decreasing order.
+
+Return the sorted array.
+
+Example 1:
+
+Input: nums = [1,1,2,2,2,3]
+Output: [3,1,1,2,2,2]
+Explanation: '3' has a frequency of 1, '1' has a frequency of 2, and '2' has a frequency of 3.
+
+Example 2:
+
+Input: nums = [2,3,1,3,2]
+Output: [1,3,3,2,2]
+Explanation: '2' and '3' both have a frequency of 2, so they are sorted in decreasing order.
+
+Example 3:
+
+Input: nums = [-1,1,-6,4,5,-6,1,4,1]
+Output: [5,-1,4,4,-6,-6,1,1,1]
+
+Constraints:
+
+    1 <= nums.length <= 100
+    -100 <= nums[i] <= 100
+
+*/
+
+class Solution {
+    public int[] frequencySort(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int n : nums) map.put(n, map.containsKey(n) ? map.get(n) + 1 : 1 );
+        return Arrays.stream(nums).boxed()
+                            			.sorted((n1, n2) -> map.get(n1) != map.get(n2) ? map.get(n1) - map.get(n2) : n2 - n1)
+                            			.mapToInt(n -> n)
+                            			.toArray();
+    }
+}
