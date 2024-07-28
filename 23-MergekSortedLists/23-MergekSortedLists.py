a07e19be2a1134f03d6db7cef5451c2c43f165b5
Akshay Bahadur <akshaybahadur21@gmail.com>
2024-07-27 21:34:51 -0700

    Create 23-MergekSortedLists.py

---
+++ 23-MergekSortedLists/23-MergekSortedLists.py
@@ -0,0 +1,64 @@
+"""
+
+23. Merge k Sorted Lists
+
+You are given an array of k linked-lists lists, each linked-list is sorted in ascending order.
+
+Merge all the linked-lists into one sorted linked-list and return it.
+
+Example 1:
+
+Input: lists = [[1,4,5],[1,3,4],[2,6]]
+Output: [1,1,2,3,4,4,5,6]
+Explanation: The linked-lists are:
+[
+  1->4->5,
+  1->3->4,
+  2->6
+]
+merging them into one sorted list:
+1->1->2->3->4->4->5->6
+
+Example 2:
+
+Input: lists = []
+Output: []
+
+Example 3:
+
+Input: lists = [[]]
+Output: []
+
+Constraints:
+
+    k == lists.length
+    0 <= k <= 104
+    0 <= lists[i].length <= 500
+    -104 <= lists[i][j] <= 104
+    lists[i] is sorted in ascending order.
+    The sum of lists[i].length will not exceed 104.
+
+"""
+
+# # Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+        
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        setattr(ListNode, "__lt__", lambda self, other: self.val <= other.val)
+        newHead = ListNode(0)
+        counter = newHead
+        heap = []
+        for head in lists:
+             while head: 
+                temp = head.next;
+                head.next = None;
+                heapq.heappush(heap, head)
+                head = temp;
+        while heap:
+            counter.next = heappop(heap);
+            counter = counter.next;
+        return newHead.next;
