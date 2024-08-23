e0973e75c9f9942f1c4f9cb3d5e549d0d7f8e80c
Akshay Bahadur <akshaybahadur21@gmail.com>
2024-08-23 00:31:41 -0700

    Create 938-RangeSumofBST.py

---
+++ 938-RangeSumofBST/938-RangeSumofBST.py
@@ -0,0 +1,44 @@
+"""
+
+938. Range Sum of BST
+
+Given the root node of a binary search tree and two integers low and high, return the sum of values of all nodes with a value in the inclusive range [low, high].
+
+Example 1:
+
+Input: root = [10,5,15,3,7,null,18], low = 7, high = 15
+Output: 32
+Explanation: Nodes 7, 10, and 15 are in the range [7, 15]. 7 + 10 + 15 = 32.
+
+Example 2:
+
+Input: root = [10,5,15,3,7,13,18,1,null,6], low = 6, high = 10
+Output: 23
+Explanation: Nodes 6, 7, and 10 are in the range [6, 10]. 6 + 7 + 10 = 23.
+
+Constraints:
+
+    The number of nodes in the tree is in the range [1, 2 * 104].
+    1 <= Node.val <= 105
+    1 <= low <= high <= 105
+    All Node.val are unique.
+
+"""
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
+        def dfs(node):
+            if not node: return
+            if node.val >= low and node.val <= high: res[0] += node.val
+            dfs(node.left)
+            dfs(node.right)
+        res = [0]
+        dfs(root)
+        return res[0]
+        
