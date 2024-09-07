c4bd2e88790de65e148a09f97ba70bc77743c5e2
Akshay Bahadur <akshaybahadur21@gmail.com>
2024-09-06 17:35:13 -0700

    Create 1367-LinkedListinBinaryTree.py

---
+++ 1367-LinkedListinBinaryTree/1367-LinkedListinBinaryTree.py
@@ -0,0 +1,58 @@
+"""
+
+1367. Linked List in Binary Tree
+
+Given a binary tree root and a linked list with head as the first node. 
+
+Return True if all the elements in the linked list starting from the head correspond to some downward path connected in the binary tree otherwise return False.
+
+In this context downward path means a path that starts at some node and goes downwards.
+
+Example 1:
+
+Input: head = [4,2,8], root = [1,4,4,null,2,2,null,1,null,6,8,null,null,null,null,1,3]
+Output: true
+Explanation: Nodes in blue form a subpath in the binary Tree.  
+
+Example 2:
+
+Input: head = [1,4,2,6], root = [1,4,4,null,2,2,null,1,null,6,8,null,null,null,null,1,3]
+Output: true
+
+Example 3:
+
+Input: head = [1,4,2,6,8], root = [1,4,4,null,2,2,null,1,null,6,8,null,null,null,null,1,3]
+Output: false
+Explanation: There is no path in the binary tree that contains all the elements of the linked list from head.
+
+Constraints:
+
+    The number of nodes in the tree will be in the range [1, 2500].
+    The number of nodes in the list will be in the range [1, 100].
+    1 <= Node.val <= 100 for each node in the linked list and binary tree.
+
+"""
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSubPath(self, head: Optional[ListNode], root: Optional[TreeNode]) -> bool:
+        def findPath(node, head):
+            if node is None: return False
+            if dfs(node, head): return True
+            return findPath(node.left, head) or findPath(node.right, head)
+        def dfs(node, head):
+            if head is None: return True
+            if node is None: return False
+            if node.val != head.val: return False
+            return dfs(node.left, head.next) or dfs(node.right, head.next)
+        return findPath(root, head)
