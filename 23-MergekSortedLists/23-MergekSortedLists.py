925f348b1ca66057b76afa009a94f40f0eabbeb3
Akshay Bahadur <akshaybahadur21@gmail.com>
2024-09-30 01:14:32 -0700

    Update 23-MergekSortedLists.py

--- 23-MergekSortedLists/23-MergekSortedLists.py
+++ 23-MergekSortedLists/23-MergekSortedLists.py
@@ -62,3 +62,30 @@ class Solution:
             counter.next = heappop(heap);
             counter = counter.next;
         return newHead.next;
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        if not lists: return None
+        def merge(l1, l2):
+            if not l1: return l2
+            if not l2: return l1
+            if l1.val <= l2.val:
+                l1.next = merge(l1.next, l2)
+                return l1
+            else:
+                l2.next = merge(l1, l2.next)
+                return l2
+        def partition(lo, hi):
+            if lo >= hi: return lists[lo]
+            mid = lo + (hi - lo) // 2
+            l1 = partition(lo, mid)
+            l2 = partition(mid + 1, hi)
+            return merge(l1, l2)
+            return merge(l1, l2);
+        return partition(0, len(lists) - 1)
