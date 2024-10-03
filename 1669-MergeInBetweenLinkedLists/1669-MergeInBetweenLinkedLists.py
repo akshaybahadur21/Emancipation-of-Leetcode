8b50c4c7daf8c5d22f86fbe4657b31ea7f623b0b
Akshay Bahadur <akshaybahadur21@gmail.com>
2024-10-02 23:55:00 -0700

    Create 1669-MergeInBetweenLinkedLists.py

---
+++ 1669-MergeInBetweenLinkedLists/1669-MergeInBetweenLinkedLists.py
@@ -0,0 +1,53 @@
+"""
+
+1669. Merge In Between Linked Lists
+
+You are given two linked lists: list1 and list2 of sizes n and m respectively.
+
+Remove list1's nodes from the ath node to the bth node, and put list2 in their place.
+
+The blue edges and nodes in the following figure indicate the result:
+
+Build the result list and return its head.
+
+Example 1:
+
+Input: list1 = [10,1,13,6,9,5], a = 3, b = 4, list2 = [1000000,1000001,1000002]
+Output: [10,1,13,1000000,1000001,1000002,5]
+Explanation: We remove the nodes 3 and 4 and put the entire list2 in their place. The blue edges and nodes in the above figure indicate the result.
+
+Example 2:
+
+Input: list1 = [0,1,2,3,4,5,6], a = 2, b = 5, list2 = [1000000,1000001,1000002,1000003,1000004]
+Output: [0,1,1000000,1000001,1000002,1000003,1000004,6]
+Explanation: The blue edges and nodes in the above figure indicate the result.
+
+Constraints:
+
+    3 <= list1.length <= 104
+    1 <= a <= b < list1.length - 1
+    1 <= list2.length <= 104
+
+"""
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeInBetween(self, list1: ListNode, a: int, b: int, list2: ListNode) -> ListNode:
+        newhead = ListNode()
+        temp = newhead
+        for i in range(a):
+            temp.next = ListNode(list1.val)
+            list1 = list1.next
+            temp = temp.next
+        while list2:
+            temp.next = ListNode(list2.val)
+            temp = temp.next
+            list2 = list2.next
+        for i in range(b - a):
+            list1 = list1.next
+        temp.next = list1.next
+        return newhead.next
