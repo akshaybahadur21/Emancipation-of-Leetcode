6dbc7f403309cf38d7d26b0b3abbd04b18ba3511
Akshay Bahadur <akshaybahadur21@gmail.com>
2024-10-04 00:25:36 -0700

    Create 129-SumRoottoLeafNumbers.py

---
+++ 129-SumRoottoLeafNumbers/129-SumRoottoLeafNumbers.py
@@ -0,0 +1,62 @@
+"""
+
+129. Sum Root to Leaf Numbers
+
+You are given the root of a binary tree containing digits from 0 to 9 only.
+
+Each root-to-leaf path in the tree represents a number.
+
+    For example, the root-to-leaf path 1 -> 2 -> 3 represents the number 123.
+
+Return the total sum of all root-to-leaf numbers. Test cases are generated so that the answer will fit in a 32-bit integer.
+
+A leaf node is a node with no children.
+
+Example 1:
+
+Input: root = [1,2,3]
+Output: 25
+Explanation:
+The root-to-leaf path 1->2 represents the number 12.
+The root-to-leaf path 1->3 represents the number 13.
+Therefore, sum = 12 + 13 = 25.
+
+Example 2:
+
+Input: root = [4,9,0,5,1]
+Output: 1026
+Explanation:
+The root-to-leaf path 4->9->5 represents the number 495.
+The root-to-leaf path 4->9->1 represents the number 491.
+The root-to-leaf path 4->0 represents the number 40.
+Therefore, sum = 495 + 491 + 40 = 1026.
+
+Constraints:
+
+    The number of nodes in the tree is in the range [1, 1000].
+    0 <= Node.val <= 9
+    The depth of the tree will not exceed 10.
+
+"""
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sumNumbers(self, root: Optional[TreeNode]) -> int:
+        res = [0]
+        def dfs(node, curr):
+            if not node: return
+            if not node.left and not node.right:
+                curr += str(node.val)
+                res[0] += int(curr)
+                return
+            dfs(node.left, curr + str(node.val))
+            dfs(node.right, curr + str(node.val))
+        dfs(root, "")
+        return res[0]
+        
