427d6a27276ea19d13beb47c1fae8cacbad40940
Akshay Bahadur <akshaybahadur21@gmail.com>
2024-10-08 12:58:31 -0700

    Update 1944-NumberofVisiblePeopleinaQueue.py

--- 1944-NumberofVisiblePeopleinaQueue/1944-NumberofVisiblePeopleinaQueue.py
+++ 1944-NumberofVisiblePeopleinaQueue/1944-NumberofVisiblePeopleinaQueue.py
@@ -45,3 +45,16 @@ class Solution:
             if stack: res[i] += 1
             stack.append(h)
         return res[::-1]
+
+
+class Solution:
+    def canSeePersonsCount(self, heights: List[int]) -> List[int]:
+        stack = []
+        res = [0] * len(heights)
+        for i, h in enumerate(heights):
+            while stack and heights[stack[-1]] <= h:
+                res[stack.pop()] += 1
+            if stack:
+                res[stack[-1]] += 1
+            stack.append(i)
+        return res 
