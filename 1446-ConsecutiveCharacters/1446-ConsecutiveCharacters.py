6a298b4cc74fcfa6325dc931c088969d81567cb0
Akshay Bahadur <akshaybahadur21@gmail.com>
2025-02-02 13:33:31 -0800

    Create 1446-ConsecutiveCharacters.py

---
+++ 1446-ConsecutiveCharacters/1446-ConsecutiveCharacters.py
@@ -0,0 +1,38 @@
+"""
+
+1446. Consecutive Characters
+
+The power of the string is the maximum length of a non-empty substring that contains only one unique character.
+
+Given a string s, return the power of s.
+
+Example 1:
+
+Input: s = "leetcode"
+Output: 2
+Explanation: The substring "ee" is of length 2 with the character 'e' only.
+
+Example 2:
+
+Input: s = "abbcccddddeeeeedcba"
+Output: 5
+Explanation: The substring "eeeee" is of length 5 with the character 'e' only.
+
+Constraints:
+
+    1 <= s.length <= 500
+    s consists of only lowercase English letters.
+
+"""
+
+class Solution:
+    def maxPower(self, s: str) -> int:
+        res = 0
+        temp = 1
+        for i in range(len(s) -1):
+            if s[i] == s[i + 1]:
+                temp += 1
+            else:
+                res = max(res, temp)
+                temp = 1
+        return max(res, temp)
