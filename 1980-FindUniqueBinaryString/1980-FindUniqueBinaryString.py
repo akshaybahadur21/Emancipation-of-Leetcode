dd368be8503c78743b45866655e741e9259c81db
Akshay Bahadur <akshaybahadur21@gmail.com>
2025-02-19 23:18:41 -0800

    Create 1980-FindUniqueBinaryString.py

---
+++ 1980-FindUniqueBinaryString/1980-FindUniqueBinaryString.py
@@ -0,0 +1,46 @@
+"""
+
+1980. Find Unique Binary String
+
+Given an array of strings nums containing n unique binary strings each of length n, return a binary string of length n that does not appear in nums. If there are multiple answers, you may return any of them.
+
+Example 1:
+
+Input: nums = ["01","10"]
+Output: "11"
+Explanation: "11" does not appear in nums. "00" would also be correct.
+
+Example 2:
+
+Input: nums = ["00","01"]
+Output: "11"
+Explanation: "11" does not appear in nums. "10" would also be correct.
+
+Example 3:
+
+Input: nums = ["111","011","001"]
+Output: "101"
+Explanation: "101" does not appear in nums. "000", "010", "100", and "110" would also be correct.
+
+Constraints:
+
+    n == nums.length
+    1 <= n <= 16
+    nums[i].length == n
+    nums[i] is either '0' or '1'.
+    All the strings of nums are unique.
+
+"""
+
+class Solution:
+    def findDifferentBinaryString(self, nums: List[str]) -> str:
+        vis = set(nums)
+        def dfs(curr):
+            if len(curr) == len(nums[0]):
+                if curr not in vis:
+                    return curr
+                return ""
+            for nextt in ["0", "1"]:
+                val = dfs(curr + nextt)
+                if val: return val
+        return dfs("")
