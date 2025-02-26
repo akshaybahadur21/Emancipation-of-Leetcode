3243c560c4cbc2a3b38d0d0503d5f10e7c85df13
Akshay Bahadur <akshaybahadur21@gmail.com>
2025-02-25 23:35:42 -0800

    Create 3063-LinkedListFrequency.py

---
+++ 3063-LinkedListFrequency/3063-LinkedListFrequency.py
@@ -0,0 +1,58 @@
+"""
+
+3063. Linked List Frequency
+
+Given the head of a linked list containing k distinct elements, return the head to a linked list of length k containing the
+
+of each distinct element in the given linked list in any order.
+
+Example 1:
+
+Input: head = [1,1,2,1,2,3]
+
+Output: [3,2,1]
+
+Explanation: There are 3 distinct elements in the list. The frequency of 1 is 3, the frequency of 2 is 2 and the frequency of 3 is 1. Hence, we return 3 -> 2 -> 1.
+
+Note that 1 -> 2 -> 3, 1 -> 3 -> 2, 2 -> 1 -> 3, 2 -> 3 -> 1, and 3 -> 1 -> 2 are also valid answers.
+
+Example 2:
+
+Input: head = [1,1,2,2,2]
+
+Output: [2,3]
+
+Explanation: There are 2 distinct elements in the list. The frequency of 1 is 2 and the frequency of 2 is 3. Hence, we return 2 -> 3.
+
+Example 3:
+
+Input: head = [6,5,4,3,2,1]
+
+Output: [1,1,1,1,1,1]
+
+Explanation: There are 6 distinct elements in the list. The frequency of each of them is 1. Hence, we return 1 -> 1 -> 1 -> 1 -> 1 -> 1.
+
+Constraints:
+
+    The number of nodes in the list is in the range [1, 105].
+    1 <= Node.val <= 105
+
+"""
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def frequenciesOfElements(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        mapp = defaultdict(int)
+        while head:
+            mapp[head.val] += 1
+            head = head.next
+        newhead = ListNode(0)
+        head = newhead
+        for k, v in mapp.items():
+            head.next = ListNode(v)
+            head = head.next
+        return newhead.next
