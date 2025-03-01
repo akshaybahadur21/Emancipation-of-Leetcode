bef8db73a576dbf2779ec9b2c2bf1920bc977aa7
Akshay Bahadur <akshaybahadur21@gmail.com>
2025-03-01 13:39:46 -0800

    Create 559-MaximumDepthofN-aryTree.py

---
+++ 559-MaximumDepthofN-aryTree/559-MaximumDepthofN-aryTree.py
@@ -0,0 +1,44 @@
+"""
+
+559. Maximum Depth of N-ary Tree
+
+Given a n-ary tree, find its maximum depth.
+
+The maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+
+Nary-Tree input serialization is represented in their level order traversal, each group of children is separated by the null value (See examples).
+
+Example 1:
+
+Input: root = [1,null,3,2,4,null,5,6]
+Output: 3
+
+Example 2:
+
+Input: root = [1,null,2,3,4,5,null,null,6,7,null,8,null,9,10,null,null,11,null,12,null,13,null,null,14]
+Output: 5
+
+Constraints:
+
+    The total number of nodes is in the range [0, 104].
+    The depth of the n-ary tree is less than or equal to 1000.
+
+"""
+
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: Optional[int] = None, children: Optional[List['Node']] = None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def maxDepth(self, root: 'Node') -> int:
+        def dfs(node):
+            if not node: return 0
+            res = 0
+            for child in node.children:
+                res = max(res, dfs(child))
+            return res + 1
+        return dfs(root)
