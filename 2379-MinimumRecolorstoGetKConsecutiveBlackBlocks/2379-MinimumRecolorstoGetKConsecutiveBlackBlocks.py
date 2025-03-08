9b9928ff7f17bb7c848b6986184d541208b3b761
Akshay Bahadur <akshaybahadur21@gmail.com>
2025-03-07 18:15:47 -0800

    Create 2379-MinimumRecolorstoGetKConsecutiveBlackBlocks.py

---
+++ 2379-MinimumRecolorstoGetKConsecutiveBlackBlocks/2379-MinimumRecolorstoGetKConsecutiveBlackBlocks.py
@@ -0,0 +1,53 @@
+"""
+
+2379. Minimum Recolors to Get K Consecutive Black Blocks
+
+You are given a 0-indexed string blocks of length n, where blocks[i] is either 'W' or 'B', representing the color of the ith block. The characters 'W' and 'B' denote the colors white and black, respectively.
+
+You are also given an integer k, which is the desired number of consecutive black blocks.
+
+In one operation, you can recolor a white block such that it becomes a black block.
+
+Return the minimum number of operations needed such that there is at least one occurrence of k consecutive black blocks.
+
+Example 1:
+
+Input: blocks = "WBBWWBBWBW", k = 7
+Output: 3
+Explanation:
+One way to achieve 7 consecutive black blocks is to recolor the 0th, 3rd, and 4th blocks
+so that blocks = "BBBBBBBWBW". 
+It can be shown that there is no way to achieve 7 consecutive black blocks in less than 3 operations.
+Therefore, we return 3.
+
+Example 2:
+
+Input: blocks = "WBWBBBW", k = 2
+Output: 0
+Explanation:
+No changes need to be made, since 2 consecutive black blocks already exist.
+Therefore, we return 0.
+
+Constraints:
+
+    n == blocks.length
+    1 <= n <= 100
+    blocks[i] is either 'W' or 'B'.
+    1 <= k <= n
+
+"""
+
+class Solution:
+    def minimumRecolors(self, blocks: str, k: int) -> int:
+        lo, hi = 0, 0
+        whites = 0
+        res = float("inf")
+        while hi < len(blocks):
+            if blocks[hi] == 'W': whites += 1
+            if hi - lo + 1 == k:
+                res = min(res, whites)
+                if blocks[lo] == 'W':
+                    whites -= 1
+                lo += 1
+            hi += 1
+        return res
