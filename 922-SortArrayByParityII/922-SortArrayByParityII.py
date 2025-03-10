998c750d44e7a0319c5ff6cd5fafa39f4651e0e9
Akshay Bahadur <akshaybahadur21@gmail.com>
2025-03-09 17:40:52 -0700

    Create 922-SortArrayByParityII.py

---
+++ 922-SortArrayByParityII/922-SortArrayByParityII.py
@@ -0,0 +1,43 @@
+"""
+
+922. Sort Array By Parity II
+
+Given an array of integers nums, half of the integers in nums are odd, and the other half are even.
+
+Sort the array so that whenever nums[i] is odd, i is odd, and whenever nums[i] is even, i is even.
+
+Return any answer array that satisfies this condition.
+
+Example 1:
+
+Input: nums = [4,2,5,7]
+Output: [4,5,2,7]
+Explanation: [4,7,2,5], [2,5,4,7], [2,7,4,5] would also have been accepted.
+
+Example 2:
+
+Input: nums = [2,3]
+Output: [2,3]
+
+Constraints:
+
+    2 <= nums.length <= 2 * 104
+    nums.length is even.
+    Half of the integers in nums are even.
+    0 <= nums[i] <= 1000
+
+Follow Up: Could you solve it in-place?
+
+"""
+
+class Solution:
+    def sortArrayByParityII(self, nums: List[int]) -> List[int]:
+        odd_idx = 1
+        for even_idx in range(0, len(nums), 2):
+            if nums[even_idx] % 2 != 0:
+                while nums[odd_idx] % 2 != 0 :
+                    odd_idx += 2
+                temp = nums[even_idx]
+                nums[even_idx] = nums[odd_idx]
+                nums[odd_idx] = temp
+        return nums
