b5cb15ecac0834acb4c01f6e60f5c886b49bbb45
Akshay Bahadur <akshaybahadur21@gmail.com>
2025-03-28 14:18:16 -0700

    Update 129-SumRoottoLeafNumbers.py

--- 129-SumRoottoLeafNumbers/129-SumRoottoLeafNumbers.py
+++ 129-SumRoottoLeafNumbers/129-SumRoottoLeafNumbers.py
@@ -59,4 +59,24 @@ class Solution:
             dfs(node.right, curr + str(node.val))
         dfs(root, "")
         return res[0]
-        
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sumNumbers(self, root: Optional[TreeNode]) -> int:
+        res = [0]
+        def dfs(node, curr):
+            if not node: return
+            if not node.left and not node.right:
+                res[0] += curr * 10 + node.val
+                return
+            dfs(node.left, curr * 10 + node.val)
+            dfs(node.right, curr * 10 + node.val)
+        dfs(root, 0)
+        return res[0]
+
