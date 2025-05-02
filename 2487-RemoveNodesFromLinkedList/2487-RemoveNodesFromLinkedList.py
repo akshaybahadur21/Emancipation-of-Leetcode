fa638e351335a15f2c42eba4c1bef2b69ba55427
Akshay Bahadur <akshaybahadur21@gmail.com>
2025-05-01 21:30:16 -0700

    Create 2487-RemoveNodesFromLinkedList.py

---
+++ 2487-RemoveNodesFromLinkedList/2487-RemoveNodesFromLinkedList.py
@@ -0,0 +1,54 @@
+"""
+
+2487. Remove Nodes From Linked List
+
+You are given the head of a linked list.
+
+Remove every node which has a node with a greater value anywhere to the right side of it.
+
+Return the head of the modified linked list.
+
+Example 1:
+
+Input: head = [5,2,13,3,8]
+Output: [13,8]
+Explanation: The nodes that should be removed are 5, 2 and 3.
+- Node 13 is to the right of node 5.
+- Node 13 is to the right of node 2.
+- Node 8 is to the right of node 3.
+
+Example 2:
+
+Input: head = [1,1,1,1]
+Output: [1,1,1,1]
+Explanation: Every node has value 1, so no nodes are removed.
+
+Constraints:
+
+    The number of the nodes in the given list is in the range [1, 105].
+    1 <= Node.val <= 105
+
+"""
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def removeNodes(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        s = deque()
+        newhead = ListNode(0)
+        temp = newhead
+        while head:
+            if len(s) == 0:  s.append(head.val)
+            else:
+                if head.val > s[-1]:
+                    s.pop()
+                    continue
+                else: s.append(head.val)
+            head = head.next
+        while s:
+            temp.next = ListNode(s.popleft())
+            temp = temp.next
+        return newhead.next
