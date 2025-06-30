3fc46cbbbd7e57e00cfae3eab7c2b7fe8f6fb132
Akshay Bahadur <akshaybahadur21@gmail.com>
2025-06-29 17:30:29 -0700

    Update 594-LongestHarmoniousSubsequence.java

--- 594-LongestHarmoniousSubsequence/594-LongestHarmoniousSubsequence.java
+++ 594-LongestHarmoniousSubsequence/594-LongestHarmoniousSubsequence.java
@@ -44,3 +44,13 @@ class Solution {
         return max;
     }
 }
+
+
+class Solution:
+    def findLHS(self, nums: List[int]) -> int:
+        mapp = Counter(nums)
+        res = 0
+        for k, v in mapp.items():
+            if (k + 1) in mapp:
+                res = max(res, mapp[k] + mapp[k + 1])
+        return res
