e43fe1a1b00ac28b4ef8b7c350c227f6b4a560af
Akshay Bahadur <akshaybahadur21@gmail.com>
2025-09-27 15:23:43 -0700

    Create 628-MaximumProductofThreeNumbers.py

---
+++ 628-MaximumProductofThreeNumbers/628-MaximumProductofThreeNumbers.py
@@ -0,0 +1,33 @@
+"""
+
+628. Maximum Product of Three Numbers
+
+Given an integer array nums, find three numbers whose product is maximum and return the maximum product.
+
+Example 1:
+
+Input: nums = [1,2,3]
+Output: 6
+
+Example 2:
+
+Input: nums = [1,2,3,4]
+Output: 24
+
+Example 3:
+
+Input: nums = [-1,-2,-3]
+Output: -6
+
+Constraints:
+
+    3 <= nums.length <= 104
+    -1000 <= nums[i] <= 1000
+
+"""
+
+class Solution:
+    def maximumProduct(self, nums: List[int]) -> int:
+        nums = sorted(nums)
+        return max  (nums[0] * nums[1] * nums[-1], 
+                    nums[-1] * nums[-2] * nums[-3])
