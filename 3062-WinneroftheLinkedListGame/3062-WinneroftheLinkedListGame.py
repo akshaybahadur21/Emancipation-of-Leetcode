39fac906d14e7b3b8e399a2f2a5279547ead5e41
Akshay Bahadur <akshaybahadur21@gmail.com>
2025-09-27 22:38:06 -0700

    Create 3062-WinneroftheLinkedListGame.py

---
+++ 3062-WinneroftheLinkedListGame/3062-WinneroftheLinkedListGame.py
@@ -0,0 +1,90 @@
+"""
+
+3062. Winner of the Linked List Game
+
+You are given the head of a linked list of even length containing integers.
+
+Each odd-indexed node contains an odd integer and each even-indexed node contains an even integer.
+
+We call each even-indexed node and its next node a pair, e.g., the nodes with indices 0 and 1 are a pair, the nodes with indices 2 and 3 are a pair, and so on.
+
+For every pair, we compare the values of the nodes in the pair:
+
+    If the odd-indexed node is higher, the "Odd" team gets a point.
+    If the even-indexed node is higher, the "Even" team gets a point.
+
+Return the name of the team with the higher points, if the points are equal, return "Tie".
+
+Example 1:
+
+Input: head = [2,1]
+
+Output: "Even"
+
+Explanation: There is only one pair in this linked list and that is (2,1). Since 2 > 1, the Even team gets the point.
+
+Hence, the answer would be "Even".
+
+Example 2:
+
+Input: head = [2,5,4,7,20,5]
+
+Output: "Odd"
+
+Explanation: There are 3 pairs in this linked list. Let's investigate each pair individually:
+
+(2,5) -> Since 2 < 5, The Odd team gets the point.
+
+(4,7) -> Since 4 < 7, The Odd team gets the point.
+
+(20,5) -> Since 20 > 5, The Even team gets the point.
+
+The Odd team earned 2 points while the Even team got 1 point and the Odd team has the higher points.
+
+Hence, the answer would be "Odd".
+
+Example 3:
+
+Input: head = [4,5,2,1]
+
+Output: "Tie"
+
+Explanation: There are 2 pairs in this linked list. Let's investigate each pair individually:
+
+(4,5) -> Since 4 < 5, the Odd team gets the point.
+
+(2,1) -> Since 2 > 1, the Even team gets the point.
+
+Both teams earned 1 point.
+
+Hence, the answer would be "Tie".
+
+Constraints:
+
+    The number of nodes in the list is in the range [2, 100].
+    The number of nodes in the list is even.
+    1 <= Node.val <= 100
+    The value of each odd-indexed node is odd.
+    The value of each even-indexed node is even.
+
+"""
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def gameResult(self, head: Optional[ListNode]) -> str:
+        even, odd = head, head.next
+        ecount, ocount = 0, 0
+        while odd:
+            if even.val > odd.val:
+                ecount += 1
+            else:
+                ocount += 1
+            even = even.next.next
+            if not odd.next: break
+            odd = odd.next.next
+        if ecount == ocount: return "Tie"
+        return "Even" if ecount > ocount else "Odd"
