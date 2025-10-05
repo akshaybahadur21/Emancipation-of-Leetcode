c31fc08fe1b1300410708ee1f60c1117ce835f26
Akshay Bahadur <akshaybahadur21@gmail.com>
2025-10-04 20:01:27 -0700

    Create 953-VerifyinganAlienDictionary.py

---
+++ 953-VerifyinganAlienDictionary/953-VerifyinganAlienDictionary.py
@@ -0,0 +1,49 @@
+"""
+
+953. Verifying an Alien Dictionary
+
+In an alien language, surprisingly, they also use English lowercase letters, but possibly in a different order. The order of the alphabet is some permutation of lowercase letters.
+
+Given a sequence of words written in the alien language, and the order of the alphabet, return true if and only if the given words are sorted lexicographically in this alien language.
+
+Example 1:
+
+Input: words = ["hello","leetcode"], order = "hlabcdefgijkmnopqrstuvwxyz"
+Output: true
+Explanation: As 'h' comes before 'l' in this language, then the sequence is sorted.
+
+Example 2:
+
+Input: words = ["word","world","row"], order = "worldabcefghijkmnpqstuvxyz"
+Output: false
+Explanation: As 'd' comes after 'l' in this language, then words[0] > words[1], hence the sequence is unsorted.
+
+Example 3:
+
+Input: words = ["apple","app"], order = "abcdefghijklmnopqrstuvwxyz"
+Output: false
+Explanation: The first three characters "app" match, and the second string is shorter (in size.) According to lexicographical rules "apple" > "app", because 'l' > '∅', where '∅' is defined as the blank character which is less than any other character (More info).
+
+Constraints:
+
+    1 <= words.length <= 100
+    1 <= words[i].length <= 20
+    order.length == 26
+    All characters in words[i] and order are English lowercase letters.
+
+"""
+
+class Solution:
+    def isAlienSorted(self, words: List[str], order: str) -> bool:
+        mapp = defaultdict(int)
+        for i, ch in enumerate(order): mapp[ch] = i
+        def compare(w1, w2):
+            l1 = len(w1)
+            l2 = len(w2)
+            for i in range(min(l1, l2)):
+                if w1[i] != w2[i]:
+                    return mapp[w1[i]] > mapp[w2[i]]
+            return l1 > l2
+        for i in range(1, len(words)):
+            if compare(words[i - 1], words[i]): return False
+        return True
