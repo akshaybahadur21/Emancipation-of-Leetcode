5ab0baed701092759b0e030fc683d2570f91fa49
Akshay Bahadur <akshaybahadur21@gmail.com>
2026-02-17 22:23:07 -0800

    Create 693-BinaryNumberwithAlternatingBits.py

---
+++ 693-BinaryNumberwithAlternatingBits/693-BinaryNumberwithAlternatingBits.py
@@ -0,0 +1,37 @@
+"""
+
+693. Binary Number with Alternating Bits
+
+Given a positive integer, check whether it has alternating bits: namely, if two adjacent bits will always have different values.
+
+Example 1:
+
+Input: n = 5
+Output: true
+Explanation: The binary representation of 5 is: 101
+
+Example 2:
+
+Input: n = 7
+Output: false
+Explanation: The binary representation of 7 is: 111.
+
+Example 3:
+
+Input: n = 11
+Output: false
+Explanation: The binary representation of 11 is: 1011.
+
+Constraints:
+
+    1 <= n <= 231 - 1
+
+"""
+
+class Solution:
+    def hasAlternatingBits(self, n: int) -> bool:
+        bits = bin(n)
+        for i in range(len(bits) - 1):
+            if bits[i] == bits[i + 1]:
+                return False
+        return True
