0482f48ec4b865aaa90cc99e417d127295477dd7
Akshay Bahadur <akshaybahadur21@gmail.com>
2026-03-08 18:45:41 -0700

    Add solution for Largest Even Number problem
    Implement solution for finding largest even number from a string of '1's and '2's.

---
+++ 3798-LargestEvenNumber/3798-LargestEvenNumber.py
@@ -0,0 +1,56 @@
+"""
+
+3798. Largest Even Number
+
+You are given a string s consisting only of the characters '1' and '2'.
+
+You may delete any number of characters from s without changing the order of the remaining characters.
+
+Return the largest possible resultant string that represents an even integer. If there is no such string, return the empty string "".
+
+Example 1:
+
+Input: s = "1112"
+
+Output: "1112"
+
+Explanation:
+
+The string already represents the largest possible even number, so no deletions are needed.
+
+Example 2:
+
+Input: s = "221"
+
+Output: "22"
+
+Explanation:
+
+Deleting '1' results in the largest possible even number which is equal to 22.
+
+Example 3:
+
+Input: s = "1"
+
+Output: ""
+
+Explanation:
+
+There is no way to get an even number.
+
+Constraints:
+
+    1 <= s.length <= 100
+    s consists only of the characters '1' and '2'.
+
+"""
+
+class Solution:
+    def largestEven(self, s: str) -> str:
+        hi = len(s) - 1
+        while hi >= 0:
+            if int(s[hi]) % 2 == 0:
+                return s[0: hi + 1]
+            else:
+                hi -= 1
+        return ""
