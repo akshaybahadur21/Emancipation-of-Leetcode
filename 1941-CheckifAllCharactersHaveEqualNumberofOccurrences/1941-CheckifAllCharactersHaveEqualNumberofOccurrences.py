af0e29c0b76c394e804ea4acf27ad391a12697e8
Akshay Bahadur <akshaybahadur21@gmail.com>
2026-04-11 11:41:15 -0700

    Create 1941-CheckifAllCharactersHaveEqualNumberofOccurrences.py

---
+++ 1941-CheckifAllCharactersHaveEqualNumberofOccurrences/1941-CheckifAllCharactersHaveEqualNumberofOccurrences.py
@@ -0,0 +1,35 @@
+"""
+
+1941. Check if All Characters Have Equal Number of Occurrences
+
+Given a string s, return true if s is a good string, or false otherwise.
+
+A string s is good if all the characters that appear in s have the same number of occurrences (i.e., the same frequency).
+
+Example 1:
+
+Input: s = "abacbc"
+Output: true
+Explanation: The characters that appear in s are 'a', 'b', and 'c'. All characters occur 2 times in s.
+
+Example 2:
+
+Input: s = "aaabb"
+Output: false
+Explanation: The characters that appear in s are 'a' and 'b'.
+'a' occurs 3 times while 'b' occurs 2 times, which is not the same number of times.
+
+Constraints:
+
+    1 <= s.length <= 1000
+    s consists of lowercase English letters.
+
+"""
+
+class Solution:
+    def areOccurrencesEqual(self, s: str) -> bool:
+        mapp = Counter(s)
+        val = mapp.get(s[0])
+        for k, v in mapp.items():
+            if v != val: return False
+        return True
