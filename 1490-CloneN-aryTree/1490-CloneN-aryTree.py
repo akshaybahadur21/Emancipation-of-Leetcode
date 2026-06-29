de9a23b2f8f3540638dc2de76379eabe72400ac1
Akshay Bahadur <akshaybahadur21@gmail.com>
2026-06-28 21:52:59 -0700

    Create 1490-CloneN-aryTree.py

---
+++ 1490-CloneN-aryTree/1490-CloneN-aryTree.py
@@ -0,0 +1,55 @@
+"""
+
+1490. Clone N-ary Tree
+
+Given a root of an N-ary tree, return a deep copy (clone) of the tree.
+
+Each node in the n-ary tree contains a val (int) and a list (List[Node]) of its children.
+
+class Node {
+    public int val;
+    public List<Node> children;
+}
+
+Nary-Tree input serialization is represented in their level order traversal, each group of children is separated by the null value (See examples).
+
+Example 1:
+
+Input: root = [1,null,3,2,4,null,5,6]
+Output: [1,null,3,2,4,null,5,6]
+
+Example 2:
+
+Input: root = [1,null,2,3,4,5,null,null,6,7,null,8,null,9,10,null,null,11,null,12,null,13,null,null,14]
+Output: [1,null,2,3,4,5,null,null,6,7,null,8,null,9,10,null,null,11,null,12,null,13,null,null,14]
+
+ 
+
+Constraints:
+
+    The depth of the n-ary tree is less than or equal to 1000.
+    The total number of nodes is between [0, 104].
+
+ 
+Follow up: Can your solution work for the graph problem?
+
+"""
+
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: Optional[int] = None, children: Optional[List['Node']] = None):
+        self.val = val
+        self.children = children if children is not None else []
+"""
+
+class Solution:
+    def cloneTree(self, root: 'Node') -> 'Node':
+        def dfs(node):
+            if not node: return node
+            clonenode = Node(node.val)
+            for child in node.children:
+                clonenode.children.append(dfs(child))
+            return clonenode
+        if not root: return root
+        return dfs(root)
