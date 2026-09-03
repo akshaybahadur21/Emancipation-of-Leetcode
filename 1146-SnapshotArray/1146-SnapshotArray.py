044240e790f84b70c29e0f2ac2255d3dba61cf27
Akshay Bahadur <akshaybahadur21@gmail.com>
2026-09-03 10:52:58 -0700

    Clarify memory limit issue in snap method
    Added comment to clarify memory limit issue.

---
+++ 1146-SnapshotArray/1146-SnapshotArray.py
@@ -0,0 +1,62 @@
+"""
+
+1146. Snapshot Array
+
+Implement a SnapshotArray that supports the following interface:
+
+    SnapshotArray(int length) initializes an array-like data structure with the given length. Initially, each element equals 0.
+    void set(index, val) sets the element at the given index to be equal to val.
+    int snap() takes a snapshot of the array and returns the snap_id: the total number of times we called snap() minus 1.
+    int get(index, snap_id) returns the value at the given index, at the time we took the snapshot with the given snap_id
+
+Example 1:
+
+Input: ["SnapshotArray","set","snap","set","get"]
+[[3],[0,5],[],[0,6],[0,0]]
+Output: [null,null,0,null,5]
+Explanation: 
+SnapshotArray snapshotArr = new SnapshotArray(3); // set the length to be 3
+snapshotArr.set(0,5);  // Set array[0] = 5
+snapshotArr.snap();  // Take a snapshot, return snap_id = 0
+snapshotArr.set(0,6);
+snapshotArr.get(0,0);  // Get the value of array[0] with snap_id = 0, return 5
+
+ 
+
+Constraints:
+
+    1 <= length <= 5 * 104
+    0 <= index < length
+    0 <= val <= 109
+    0 <= snap_id < (the total number of times we call snap())
+    At most 5 * 104 calls will be made to set, snap, and get.
+
+"""
+
+class SnapshotArray:
+
+    def __init__(self, length: int):
+        self.id = 0
+        self.length = length
+        self.mapp = defaultdict(list)
+        self.mapp[self.id] = [0] * length
+
+    def set(self, index: int, val: int) -> None:
+        self.mapp[self.id][index] = val
+        
+
+    def snap(self) -> int:
+        self.id += 1
+        self.mapp[self.id] = self.mapp[self.id - 1].copy() # MLE
+        return self.id - 1
+
+    def get(self, index: int, snap_id: int) -> int:
+        return self.mapp[snap_id][index]
+        
+
+
+# Your SnapshotArray object will be instantiated and called as such:
+# obj = SnapshotArray(length)
+# obj.set(index,val)
+# param_2 = obj.snap()
+# param_3 = obj.get(index,snap_id)
