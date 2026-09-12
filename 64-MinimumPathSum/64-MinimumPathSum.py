dd0428fb5888c64e574ae45da152d92e1204417b
Akshay Bahadur <akshaybahadur21@gmail.com>
2026-09-12 14:50:48 -0700

    Implement dynamic programming for minimum path sum

--- 64-MinimumPathSum/64-MinimumPathSum.py
+++ 64-MinimumPathSum/64-MinimumPathSum.py
@@ -36,3 +36,14 @@ class Solution:
             cache[i][j] = grid[i][j] + min(dfs(i + 1, j), dfs(i, j + 1))
             return cache[i][j]
         return dfs(0, 0)
+
+class Solution:
+    def minPathSum(self, grid: List[List[int]]) -> int:
+        dp = [[inf] * (len(grid[0]) + 1) for _ in range(len(grid) + 1)]
+        for i in range(len(grid) - 1, -1, -1):
+            for j in range(len(grid[0]) - 1, -1, -1):
+                if i == len(grid) - 1 and j == len(grid[0]) -  1:
+                    dp[i][j] = grid[i][j]
+                else:
+                    dp[i][j] = grid[i][j] + min(dp[i + 1][j], dp[i][j + 1])
+        return dp[0][0]
