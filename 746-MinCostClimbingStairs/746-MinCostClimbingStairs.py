a51a19751e818ee62fd78fa8e3c9262316442b45
Akshay Bahadur <akshaybahadur21@gmail.com>
2026-09-11 23:17:18 -0700

    Update 746-MinCostClimbingStairs.py

--- 746-MinCostClimbingStairs/746-MinCostClimbingStairs.py
+++ 746-MinCostClimbingStairs/746-MinCostClimbingStairs.py
@@ -55,3 +55,12 @@ class Solution:
             return cache[n]
         cache = [None] * len(cost)
         return min(dfs(0), dfs(1))
+
+
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        dp = [inf] * (len(cost) + 2)
+        dp[-2], dp[-1] = 0, 0
+        for i in range(len(cost) - 1, -1, -1):
+            dp[i] = cost[i] + min(dp[i + 1], dp[i + 2])
+        return min(dp[0], dp[1])
