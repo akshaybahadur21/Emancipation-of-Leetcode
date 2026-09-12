3724aebbccb174653e943ee15d99db67adbbf4c8
Akshay Bahadur <akshaybahadur21@gmail.com>
2026-09-11 19:26:16 -0700

    Update 70-ClimbingStairs.py

--- 70-ClminbingStairs/70-ClimbingStairs.py
+++ 70-ClminbingStairs/70-ClimbingStairs.py
@@ -37,3 +37,15 @@ class Solution:
         one = self.climbStairs(n - 1)
         two = self.climbStairs(n - 2)
         return one + two
+
+
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n == 1: return 1
+        dp = [inf] * (n + 1)
+        dp[0] = 0
+        dp[1] = 1
+        dp[2] = 2
+        for i in range(3, n + 1):
+            dp[i] = dp[i - 1] + dp[i - 2]
+        return dp[-1]
