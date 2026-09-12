fdb24a657ee0c0a5e03436b6a3094995151be744
Akshay Bahadur <akshaybahadur21@gmail.com>
2026-09-12 15:06:12 -0700

    Update 1143-LongestCommonSubsequence.py

--- 1143-LongestCommonSubsequence/1143-LongestCommonSubsequence.py
+++ 1143-LongestCommonSubsequence/1143-LongestCommonSubsequence.py
@@ -47,3 +47,15 @@ class Solution:
             return val
         cache = {}
         return dfs(0, 0)
+
+
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        dp = [[0] * (len(text2) + 1) for _ in range(len(text1) + 1)] 
+        for i in range(len(text1) - 1, -1, -1):
+            for j in range(len(text2) - 1, -1, -1):
+                if text1[i] == text2[j]:
+                    dp[i][j] = 1 + dp[i + 1][j + 1]
+                else:
+                    dp[i][j] = max(dp[i + 1][j], dp[i][j + 1])
+        return dp[0][0]
