e87ba9d8557535955fae344a2d6c059e5e3e2388
Akshay Bahadur <akshaybahadur21@gmail.com>
2026-09-12 14:02:41 -0700

    Implement uniquePaths method using dynamic programming
    Added a dynamic programming approach to calculate unique paths.

--- 62-UniquePaths/62-UniquePaths.py
+++ 62-UniquePaths/62-UniquePaths.py
@@ -40,3 +40,16 @@ class Solution:
             return cache[i][j]
         cache = [[None] * n for _ in range(m)]
         return dfs(0, 0)
+
+
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        dp = [[0] * (n + 1) for _ in range(m + 1)]
+        for i in range(m - 1, -1, -1):
+            for j in range(n - 1, -1, -1):
+                if i == m - 1 and j == n - 1:
+                    dp[i][j] = 1
+                else:
+                    dp[i][j] = dp[i][j + 1] + dp[i + 1][j]
+        return dp[0][0]
+            
